840aff843c54b979d381dcfbffe4e6c97e79bc96
chinsong <1006283@mymail.sutd.edu.sg>
2024-02-15 23:58:03 +0800

    use sqlite

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from data import students
+from typing import Optional
+import sqlite3
+
+data = [ (1006283, 'Gan Chin Song', 5.0), (1000000, 'Shelen Go', '4.31')]
+
+con = sqlite3.connect("simple.db")
+cur = con.cursor() # database cursor
+cur.execute("CREATE TABLE student(id, name, gpa)")
+cur.executemany("INSERT INTO student VALUES (?,?,?)", data)
+con.commit()
 
 app = FastAPI()
 
@@ -8,8 +18,14 @@ def read_root():
     return "How are you?"
 
 @app.get("/students/{student_id}")
-def find_student(student_id: str):
+def find_student(student_id: str, response: Response):
     for student in students:
         if student["id"] == student_id:
             return student
+    response.status_code = 400
     return None
+
+@app.get('/students')
+def get_students(sortBy: Optional[str] = None, limit: Optional[int] = None):
+    # TODO: if parameters are not None, sort & limit...
+    return {"message" : "correct"}
