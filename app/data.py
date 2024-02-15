22a38b95f559b275eb6ae8adca8ed60f9bc787d5
chin song <1006283@mymail.sutd.edu.sg>
2024-02-15 20:22:45 +0800

    route parameter

---
+++ app/data.py
@@ -0,0 +1,15 @@
+students = [
+    {
+        "name": "Alice",
+        "id": "1004803",
+    },
+    {
+        "name": "Bob",
+        "id": "1004529"
+    },
+    {
+        "name": "Charlie",
+        "id": "1004910",
+        "gpa": 5.0
+    }
+]
--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from data import students
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return "Hello World"
+    return "How are you?"
+
+@app.get("/students/{student_id}")
+def find_student(student_id: str):
+    for student in students:
+        if student["id"] == student_id:
+            return student
+    return None
