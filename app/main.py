1b5ea48bec4bacdefad3ee0dbb8c44c3687a9775
chinsong <1006283@mymail.sutd.edu.sg>
2024-02-16 23:42:37 +0800

    post request

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from data import students
 from typing import Optional
 import sqlite3
 from db.db import setup_database
+from model.student import Student
 
 app = FastAPI()
 # Call the setup_database function to ensure the database is ready
@@ -55,3 +56,28 @@ def get_students(response: Response, sortBy: Optional[str] = None, limit: Option
     con.close()
     
     return student_list
+
+@app.post("/students")
+def create_student(response: Response, student: Student):
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    
+    # Check if the student already exists to avoid duplicates
+    cur.execute("SELECT * FROM student WHERE id = ?", (student.id,))
+    if cur.fetchone():
+        con.close()
+        response.status_code = 400
+        return { "message": "Student already existed"}
+
+    # Insert new student into the database
+    try:
+        cur.execute("INSERT INTO student (id, name, gpa) VALUES (?, ?, ?)", (student.id, student.name, student.gpa))
+        con.commit()
+    except sqlite3.Error as e:
+        con.close()
+        response.status_code = 422
+        return {'message': f'Error when inserting record into database: {e}'}
+    
+    con.close()
+    
+    return {'message': 'successfully insert record into database'}
